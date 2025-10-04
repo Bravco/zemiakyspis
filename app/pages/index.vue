8d60f1b98c5eb8ad54ea546dfd9ce1aebab2a415
Bravco <boss.wiw@gmail.com>
2025-10-04 11:13:17 +0200

    Optimized spline loading

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <canvas ref="canvas" class="absolute inset-0 -z-1 pointer-events-none opacity-0 lg:opacity-100"></canvas>
+        <canvas
+            v-if="showCanvas"
+            ref="canvas"
+            class="absolute inset-0 -z-1 pointer-events-none"
+        ></canvas>
         <Hero/>
         <Info/>
         <Order/>
@@ -15,59 +19,97 @@
     import ScrollTrigger from "gsap/ScrollTrigger";
 
     const canvas = ref<HTMLCanvasElement | null>(null);
+    const splineApp = ref<Application | null>(null);
+    const showCanvas = ref<boolean>(false);
+
+    function initSpline() {
+        if (!canvas.value) return;
+
+        gsap.registerPlugin(ScrollTrigger);
+        
+        const spline = new Application(canvas.value);
+        splineApp.value = spline;
+
+        spline.load("https://prod.spline.design/arZD9oYwVIhDCJdl/scene.splinecode").then(() => {
+            const object = spline.findObjectByName("low-poly_potato");
+            const offset = 60;
+
+            if (!object) return;
+            
+            gsap.set(object.scale, { x: 800, y: 800, z: 800 });
+            gsap.set(object.position, { x: offset, z: -offset });
+
+            gsap.timeline({
+                scrollTrigger: {
+                    start: "top top",
+                    endTrigger: "#info",
+                    end: "bottom bottom",
+                    scrub: true,
+                    pin: canvas.value
+                },
+            })
+            .to(object.position, { x: -offset, z: offset })
+            .to(object.rotation, {  x:1, z: 2 }, 0);
+
+            gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#info",
+                    start: "bottom bottom",
+                    endTrigger: "#objednavka",
+                    end: "bottom bottom",
+                    scrub: true,
+                    pin: canvas.value
+                },
+            })
+            .to(object.position, { x: offset, z: -offset })
+            .to(object.rotation, {  x: -1  }, 0);
+
+            gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#objednavka",
+                    start: "bottom bottom",
+                    endTrigger: "#odrody",
+                    end: "bottom bottom",
+                    scrub: true,
+                    pin: canvas.value
+                },
+            })
+            .to(object.position, { x: -offset, z: offset })
+            .to(object.rotation, {  x:1, z: 2  }, 0);
+        });
+    }
+
+    function destroySpline() {
+        if (splineApp.value) {
+            splineApp.value.dispose();
+            splineApp.value = null;
+        }
+        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        ScrollTrigger.clearMatchMedia();
+        gsap.globalTimeline.clear();
+    }
+
+    const handleResize = () => {
+        showCanvas.value = window.innerWidth > 1024;
+    };
+
+    watch(showCanvas, (newValue, oldValue) => {
+        if (newValue === oldValue) return;
+
+        if (newValue) {
+            nextTick(() => initSpline());
+        } else {
+            destroySpline();
+        }
+    });
     
     onMounted(() => {
-        gsap.registerPlugin(ScrollTrigger);
+        handleResize();
+        window.addEventListener("resize", handleResize);
+    });
 
-        if (canvas.value) {
-            const spline = new Application(canvas.value);
-            spline.load("https://prod.spline.design/arZD9oYwVIhDCJdl/scene.splinecode").then(() => {
-                const object = spline.findObjectByName("low-poly_potato");
-                const offset = 60;
-                
-                if (object) {
-                    gsap.set(object.scale, { x: 800, y: 800, z: 800 });
-                    gsap.set(object.position, { x: offset, z: -offset });
-
-                    gsap.timeline({
-                        scrollTrigger: {
-                            start: "top top",
-                            endTrigger: "#info",
-                            end: "bottom bottom",
-                            scrub: true,
-                            pin: canvas.value
-                        },
-                    })
-                    .to(object.position, { x: -offset, z: offset })
-                    .to(object.rotation, {  x:1, z: 2 }, 0);
-
-                    gsap.timeline({
-                        scrollTrigger: {
-                            trigger: "#info",
-                            start: "bottom bottom",
-                            endTrigger: "#objednavka",
-                            end: "bottom bottom",
-                            scrub: true,
-                            pin: canvas.value
-                        },
-                    })
-                    .to(object.position, { x: offset, z: -offset })
-                    .to(object.rotation, {  x: -1  }, 0);
-
-                    gsap.timeline({
-                        scrollTrigger: {
-                            trigger: "#objednavka",
-                            start: "bottom bottom",
-                            endTrigger: "#odrody",
-                            end: "bottom bottom",
-                            scrub: true,
-                            pin: canvas.value
-                        },
-                    })
-                    .to(object.position, { x: -offset, z: offset })
-                    .to(object.rotation, {  x:1, z: 2  }, 0);
-                }
-            });
-        }
+    onBeforeUnmount(() => {
+        destroySpline();
+        window.removeEventListener("resize", handleResize);
     });
 </script>
